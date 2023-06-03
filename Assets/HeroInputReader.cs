b44afb9e95e50651a1d026682b4c7533f85c5a10
Илья Галкин <galkincmr@gmail.com>
2023-06-03 18:14:09 +0300

    NEW_INPUT

--- Assets/HeroInputReader.cs
+++ Assets/HeroInputReader.cs
@@ -5,21 +5,10 @@ using UnityEngine.InputSystem;
 public class HeroInputReader : MonoBehaviour
 {
     [SerializeField] private Hero _hero;
-    private HeroInputAction _inputActions;
 
-    private void Awake()
+    public void OnMovement(InputAction.CallbackContext context)
     {
-        _inputActions = new HeroInputAction();
-        _inputActions.Hero.Movement.performed += OnMovement;
-        _inputActions.Hero.Movement.canceled += OnMovement;
-    }
-    private void OnEnable()
-    {
-        _inputActions.Enable();
-    }
-    private void OnMovement(InputAction.CallbackContext context)
-    {
-        var direction = context.ReadValue<float>();
+        var direction = context.ReadValue<Vector2>();
         _hero.SetDirection(direction);
     }
 
