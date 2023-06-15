c30d9f938387c084f1b51576a02ff135511ae1e0
Илья Галкин <galkincmr@gmail.com>
2023-06-15 18:55:16 +0300

    mirror animation

--- Assets/Hero.cs
+++ Assets/Hero.cs
@@ -11,12 +11,14 @@ public class Hero : MonoBehaviour
     private Rigidbody2D _rigidbody;
     private Vector2 _direction;
     private Animator _animator;
+    private SpriteRenderer _sprite;
 
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
+        _sprite = GetComponent<SpriteRenderer>();
     }
 
     public void SetDirection(Vector2 direction)
@@ -44,6 +46,19 @@ public class Hero : MonoBehaviour
         _animator.SetBool("is-ground", isGrounded);
         _animator.SetFloat("vertical-velocity", _rigidbody.velocity.y);
         _animator.SetBool("is-running", _direction.x != 0);
+
+        UpdateSpriteDirection();
+    }
+    private void UpdateSpriteDirection()
+    {
+        if (_direction.x > 0)
+        {
+            _sprite.flipX = false;
+        }
+        else if (_direction.x < 0)
+        {
+            _sprite.flipX = true;
+        }
     }
 
     private void OnDrawGizmos()
